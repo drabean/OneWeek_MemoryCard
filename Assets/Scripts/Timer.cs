422a718fa5d2241b20e66faa547018a673e6727c
KYB <dragon@os21.com>
2022-12-22 10:29:50 +0900

    added endScene

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -10,34 +10,41 @@ public class Timer : MonoBehaviour
     public TextMeshProUGUI timeText;
     float time;
 
+    /// <summary>
+    /// 타이머를 시작합니다.
+    /// </summary>
     public void StartTimer()
     {
         timerOn = true;
     }
 
+    /// <summary>
+    /// 타이머를 종료합니다.
+    /// </summary>
     public void EndTimer()
     {
         timerOn = false;
 
     }
-    public float getTime()
-    {
-        return time;
-    }
+    /// <summary>
+    /// 타이머의 현재 시간을 가져옵니다
+    /// </summary>
+    /// <returns></returns>
+    public float getTime => time;
 
     void Update()
     {
-        if (timerOn) TimerOn();
-        else TimerStop();
+        if (timerOn) On();
+        else Stop();
     }
 
-    public void TimerOn()
+    void On()
     {
         time += Time.deltaTime;
         timeText.text = time.ToString("N2");
     }
 
-    public void TimerStop()
+    void Stop()
     {
         timeText.text = time.ToString("N2");
     }
