2a4cac4938a021cc4675d4d5e7aa602519c411b7
KYB <dragon@os21.com>
2023-02-16 17:42:07 +0900

    added effects

--- Assets/Scripts/ReadyScene/Stethoscope.cs
+++ Assets/Scripts/ReadyScene/Stethoscope.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class Stethoscope : MonoBehaviour, Object_Interactive
 {
-    Vector3 mOffset = new Vector3(-1, 1, 0);
+    public Transform targetTr;
+    public GameObject lightObject;
+    private void Update()
+    {
+        float distance = Vector2.Distance(transform.position, targetTr.position);
+        if (distance < 1) lightObject.transform.localScale = Vector3.one * 5;
+        else lightObject.transform.localScale = Vector3.one * (5/(distance));
+
+
+    }
     public void onTouchDown(Vector3 touchPos)
     {
 
     }
     public void onTouchDrag(Vector3 touchPos)
     {
-        transform.position = touchPos + mOffset;
+        transform.position = touchPos;
     }
     public void onTouchUp(Vector3 touchPos)
     {
 
     }
+    
     
 }
